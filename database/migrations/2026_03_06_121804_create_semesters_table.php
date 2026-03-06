76b543408f203348e7755f68774b8f77f046385b
Ahmed Radwan <ahmed4.radwan7@gmail.com>
2026-03-06 15:20:33 +0200

    add_migration

---
+++ database/migrations/2026_03_06_121804_create_semesters_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('semesters', function (Blueprint $table) {
+            $table->id();
+            $table->enum('semester',['1','2']);
+            $table->foreignId('year_id')->references('id')->on('years');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('semesters');
+    }
+};
